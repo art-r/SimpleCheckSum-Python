ba347c350c3ec0ce77bb5f716ed48ae0cf3cd333
Artur <ay@local.com>
2021-05-22 20:02:44 +0200

    hotfix for comparison

--- Check-The-Sum.py
+++ Check-The-Sum.py
@@ -13,11 +13,12 @@ except IndexError:
 #realCheckSum = subprocess.Popen('openssl {usedChecksum} {file}'.format(usedChecksum = checksum, file=file), shell=True)
 checkTheSum = subprocess.run(["openssl", "{}".format(checksum), "{}".format(file)], stdout=subprocess.PIPE)
 out = checkTheSum.stdout.decode('utf-8')
-if (out == shouldSum.strip()):
+generatedCheckSum = out.split("=")[1].strip()
+if (generatedCheckSum == shouldSum.strip()):
     print('SUCCESS! The checksums match each other')
-   
+
 else:
-    print('ERROR! THE CHECKSUMS DO NOT MATCH EACH OTHER!') 
+    print('ERROR! THE CHECKSUMS DO NOT MATCH EACH OTHER!')
 
 print('used method: {}'.format(checksum))
 print('system generated checksum: {}'.format(out))
