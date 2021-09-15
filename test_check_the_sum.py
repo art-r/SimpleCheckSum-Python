12793e2f99b940d3c9bd9d54a1fa1f16febaf648
artur <gtgp-a@posteo.de>
2021-09-15 16:40:17 +0200

    Change to License file as reference
    Signed-off-by: artur <gtgp-a@posteo.de>

--- test_check_the_sum.py
+++ test_check_the_sum.py
@@ -1,4 +1,9 @@
+"""
+This is the test function for check_the_sum (used by pytest)
+It will check the checksum generation by checking the checksum of the LICENSE file
+"""
+
 import check_the_sum
 
 def test_check_the_sum():
-    assert check_the_sum.check("README.md", "sha256", "27b8bec24607fae5cdfbd7e9d1008121a70f3ead93e6b5121a418dc4dc37c3df") == 1
+    assert check_the_sum.check("LICENSE", "sha256", "6b9a233c470849c3ba6fc3772467b6211c43dcf36748434ab70bc5915be308e1") == 1
